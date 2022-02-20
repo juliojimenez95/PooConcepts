3810e740ab2b487f643dee9d523d3a25b5a36e86
Julio Jimenez <JulioJimenegarcia@gmail.com>
2022-02-20 11:43:03 -0500

    first class

---
+++ Pooconcepts/Pooconcepts/Program.cs
@@ -0,0 +1,15 @@
+﻿using Pooconcepts;
+
+Console.WriteLine("Concepts Poo");
+Console.WriteLine("============");
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 5));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
